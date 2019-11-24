9761b115fd1c7fef08959efc94c4305c12436cd1
Yoren Chang <yorenchang@gmail.com>
2019-11-24 13:24:05 +0800

    Add new folder and icon

---
+++ blocks/reports/icon.js
@@ -0,0 +1,5 @@
+const icon = <svg width="100%" height="100%" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+    <path d="M640 768v512H384V768h256zm384-512v1024H768V256h256zm1024 1152v128H0V0h128v1408h1920zm-640-896v768h-256V512h256zm384-384v1152h-256V128h256z" />
+</svg>
+
+export default icon;
